f7289ad422f51fd3acf11a7b53e8904ec9018605
sakshi choudhary <sakshi.choudhary@zensar.com>
2020-07-23 11:54:11 +0530

    initial commit

---
+++ loops.js
@@ -0,0 +1,30 @@
+          
+            var i=0
+            while (i < 10) {
+               console.log ('The number is ' + i)
+               i++
+             }
+           
+             
+   var y=4
+    do{
+      console.log('the number is' + y)
+         y++
+    }
+      while(i< 10);
+       
+
+      var x = "0";
+      switch (x) {
+        
+        case 0:
+          text = "Off";
+          break;
+        case 1:
+          text = "On";
+          break;
+        default:
+          text = "No value found";
+          console.log( + x )
+      }
+      
